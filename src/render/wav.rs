53535937ca45ca160baa2cc13ba1ee766ef3a068
Lucas Escobar <lucas.an.escobar@gmail.com>
2025-06-02 05:59:33 -0400

    Update wav format to 32 bit float
    Signed-off-by: Lucas Escobar <lucas.an.escobar@gmail.com>

--- src/render/wav.rs
+++ src/render/wav.rs
@@ -1,22 +1,6 @@
 use hound::{SampleFormat, WavSpec, WavWriter};
 use std::path::Path;
 
-//pub fn save_to_wav(buffer: &[f64], sample_rate: u32, path: &Path) {
-//    let spec = WavSpec {
-//        channels: 1,
-//        sample_rate,
-//        bits_per_sample: 16,
-//        sample_format: SampleFormat::Int,
-//    };
-//
-//    let mut writer = WavWriter::create(path, spec).unwrap();
-//    for sample in buffer {
-//        let val = (sample * i16::MAX as f64) as i16;
-//        writer.write_sample(val).unwrap();
-//    }
-//    writer.finalize().unwrap();
-//}
-
 pub fn save_to_wav(
     left: &[f64],
     right: Option<&[f64]>,
@@ -27,8 +11,8 @@ pub fn save_to_wav(
     let spec = WavSpec {
         channels,
         sample_rate,
-        bits_per_sample: 16,
-        sample_format: SampleFormat::Int,
+        bits_per_sample: 32,
+        sample_format: SampleFormat::Float,
     };
 
     let mut writer = WavWriter::create(path, spec).unwrap();
@@ -41,15 +25,15 @@ pub fn save_to_wav(
                 "Left and right buffers must be the same length"
             );
             for i in 0..left.len() {
-                let l = (left[i] * i16::MAX as f64) as i16;
-                let r = (right_buf[i] * i16::MAX as f64) as i16;
+                let l = left[i] as f32;
+                let r = right_buf[i] as f32;
                 writer.write_sample(l).unwrap();
                 writer.write_sample(r).unwrap();
             }
         }
         None => {
             for &sample in left {
-                let val = (sample * i16::MAX as f64) as i16;
+                let val = sample as f32;
                 writer.write_sample(val).unwrap();
             }
         }
